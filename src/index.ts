8fa82459938c2a113874f085a82fbc7aeb902dd4
Siddhant Jain <sidjain8000@gmail.com>
2025-03-19 19:20:49 +0530

    added logger

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { Hono } from "hono";
 import { notFound, onError } from "stoker/middlewares";
+import { pinoLogger } from "./middlewares/pino-logger";
+import { PinoLogger } from "hono-pino";
 
-const app = new Hono();
+type AppBindings = {
+  Variables: {
+    logger: PinoLogger;
+  };
+};
 
+const app = new Hono<AppBindings>();
+app.use(pinoLogger());
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
