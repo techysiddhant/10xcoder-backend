35e1a7835789416045d22f608cadd3df9c93376c
Siddhant Jain <sidjain8000@gmail.com>
2025-03-21 15:08:37 +0530

    added default validation hook

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -2,9 +2,9 @@ import { pinoLogger } from "@/middlewares/pino-logger";
 import { notFound, onError } from "stoker/middlewares";
 import { AppBindings } from "./types";
 import { OpenAPIHono } from "@hono/zod-openapi";
-
+import { defaultHook } from "stoker/openapi";
 export function createRouter() {
-  return new OpenAPIHono<AppBindings>({ strict: false });
+  return new OpenAPIHono<AppBindings>({ strict: false, defaultHook });
 }
 
 export default function createApp() {
