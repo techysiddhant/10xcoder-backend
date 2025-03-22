6044740aa2eab9339b94fb1d6f3da02ea403978e
Siddhant Jain <sidjain8000@gmail.com>
2025-03-23 00:54:00 +0530

    fix: add validation in drizzle configs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -60,8 +60,20 @@ export default process.env.LOCAL_DB_PATH
       out: "./drizzle",
       driver: "d1-http",
       dbCredentials: {
-        accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-        databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-        token: process.env.CLOUDFLARE_D1_TOKEN!,
+        accountId:
+          process.env.CLOUDFLARE_ACCOUNT_ID ||
+          (() => {
+            throw new Error("CLOUDFLARE_ACCOUNT_ID is required");
+          })(),
+        databaseId:
+          process.env.CLOUDFLARE_DATABASE_ID ||
+          (() => {
+            throw new Error("CLOUDFLARE_DATABASE_ID is required");
+          })(),
+        token:
+          process.env.CLOUDFLARE_D1_TOKEN ||
+          (() => {
+            throw new Error("CLOUDFLARE_D1_TOKEN is required");
+          })(),
       },
     });
